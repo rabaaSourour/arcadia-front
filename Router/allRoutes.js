bf41c715db2e2d45d6d83cca488e20711faec54a
Rabaa sourour <so.gomgom@gmail.com>
2024-06-06 23:02:22 +0200

    Add content for the home page (Excluded images)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,10 +2,8 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/", "Habitats", "/pages/Habitats.html"),
-    new Route("/", "Contacts", "/pages/Contacts.html"),
-    new Route("/", "Services", "/pages/Services.html"),];
+    new Route("/", "Accueil", "/Pages/Home.html"),
+   ];
 
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
