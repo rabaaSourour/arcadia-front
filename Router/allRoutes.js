1107e70ef8d63f35dbc0ef42a400b2d8e1b7e449
Rabaa sourour <so.gomgom@gmail.com>
2024-06-10 23:14:01 +0200

    Edit some pages and add photos

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,13 +2,17 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/Pages/Home.html"),
-    new Route("/Service", "Service", "/Pages/Service.html"),
-    new Route("/signin", "signin", "/Pages/signin.html"),
-    new Route("/signup", "signup", "/Pages/signup.html"),
+    new Route("/home.html", "Accueil", "/Pages/Home.html"),
+    new Route("/Services.html", "Service", "/Pages/Service.html"),
+    new Route("/signin.html", "signin", "/Pages/signin.html"),
+    new Route("/signup.html", "signup", "/Pages/signup.html"),
     new Route("/reviews", "reviews", "/Pages/reviews.html"),
     new Route("/contacts", "contacts", "/Pages/contacts.html"),
-   ];
+    new Route("/habitats", "habitats", "/Pages/habitats.html"),
+    new Route("/jungle", "jungle", "/Pages/jungle.html"),
+    new Route("/savane", "savane", "/Pages/savane.html"),
+    new Route("/marais", "marais", "/Pages/marais.html")
+    ];
 
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
