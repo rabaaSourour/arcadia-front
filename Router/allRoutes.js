619118f33f1e553912754d9da13bc2742b411e2e
Rabaa sourour <so.gomgom@gmail.com>
2024-06-11 10:53:33 +0200

    Add the 'mon Compte' page and finalize the 'habitats' page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,16 +2,16 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/home.html", "Accueil", "/Pages/Home.html"),
-    new Route("/Services.html", "Service", "/Pages/Service.html"),
-    new Route("/signin.html", "signin", "/Pages/signin.html"),
-    new Route("/signup.html", "signup", "/Pages/signup.html"),
+    new Route("/home", "Accueil", "/Pages/Home.html"),
+    new Route("/Services", "Service", "/Pages/Service.html"),
+    new Route("/signin", "signin", "/Pages/signin.html"),
+    new Route("/monCompte", "monCompte", "/Pages/monCompte.html"),
     new Route("/reviews", "reviews", "/Pages/reviews.html"),
     new Route("/contacts", "contacts", "/Pages/contacts.html"),
     new Route("/habitats", "habitats", "/Pages/habitats.html"),
     new Route("/jungle", "jungle", "/Pages/jungle.html"),
     new Route("/savane", "savane", "/Pages/savane.html"),
-    new Route("/marais", "marais", "/Pages/marais.html")
+    new Route("/marais", "marais", "/Pages/marais.html"),
     ];
 
 
